3cf9a6ecb004b8dcd1d6ebbcd2329914c7a2d5fc
komariki-komariki <komaroff.ilya.s@gmail.com>
2022-02-20 20:00:13 +0300

    init commit

---
+++ Superhero.py
@@ -0,0 +1,20 @@
+import requests
+
+smart = {}
+superheroes = ['Hulk', 'Captain America', 'Thanos']
+
+def search_id (name):
+    url_search = requests.get(f'https://superheroapi.com/api/2619421814940190/search/{name}')
+    id_number = url_search.json()['results'][0]['id']
+    url = requests.get(f'https://superheroapi.com/api/2619421814940190/{id_number}/powerstats')
+    smart.setdefault(url.json()['name'], int(url.json()['intelligence']))
+
+def search_hero():
+    for superhero in superheroes:
+        search_id(superhero)
+
+def most_intelligent():
+    search_hero()
+    print(f'Самый умный - {max(smart)}')
+
+print(most_intelligent())
